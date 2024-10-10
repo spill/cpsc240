d76a8cf36faa45e9050cbccca31f889e53e13645
ry <ry@DESKTOP-32PH758.localdomain>
2024-10-10 15:40:39 -0700

    updated

--- Assignment 04/assignment04_multiple.asm
+++ Assignment 04/assignment04_multiple.asm
@@ -1,34 +1,34 @@
 section .data
-    num dw 65535
-    mul_3 dw 0
-    other dw 0
+    num dw 65535 ; 16-bit variable
+    mul_3 dw 0 ; 16-bit variable
+    other dw 0 ; 16 bit variable
 
 section .text
     global _start
 
 _start:
-    mov ax, [num]
+    mov ax, [num]  ; load the num into ax
 
 
-    mov dx, 0
-    mov bx, 3
+    mov dx, 0 ; clear dx for the divison process
+    mov bx, 3 ; set the divisor to 3
     div bx
     test dx, dx
-    jnz else_part
+    jnz else_part ; if the remainder is not 0, jump to else part
 
-    mov ax, [num]
-    mov dx, 0
-    mov bx, 5
-    div bx
+    mov ax, [num] ; reload num to ax
+    mov dx, 0 ; clear dx for division 
+    mov bx, 5 ; set the divisor to 3
+    div bx ; divide ax by bx 
     test dx, dx
     jz else_part
 
 else_part
-    mov ax, [other]
-    inc ax
+    mov ax, [other] ; load other into ax
+    inc ax ; increment the ax by 1
     mov [other], ax
 
 end_if:
-    mov eax, 60
+    mov eax, 60 ; system call for exit
     xor edi, edi
     syscall
